2adb534984d951e858b4a96d8255480e18c728cd
Rafael Oliveira <rafa.forsoft.25@gmail.com>
2023-11-24 15:46:19 -0300

    Criação de parser para as todos que vem do back

--- src/ui/repository/todo.ts
+++ src/ui/repository/todo.ts
@@ -15,7 +15,7 @@ function get({
 }: TodoRepositoryGetParams): Promise<TodoRepositoryGetOutput> {
   return fetch("/api/todos").then(async (respostaDoServidor) => {
     const todosString = await respostaDoServidor.text();
-    const todosFromServer = JSON.parse(todosString).todos;
+    const todosFromServer = parseTodosFromServer(JSON.parse(todosString)).todos;
 
     const ALL_TODOS = todosFromServer;
     const startIndex = (page - 1) * limit;
@@ -42,4 +42,42 @@ interface Todo {
   content: string;
   date: Date;
   done: boolean;
+}
+
+function parseTodosFromServer(responseBody: unknown): { todos: Array<Todos> } {
+  // eslint-disable-next-line no-console
+  console.log("responseBOdy: ", responseBody);
+
+  if (
+    responseBody !== null &&
+    typeof responseBody === "object" &&
+    "todos" in responseBody &&
+    Array.isArray(responseBody.todos)
+  ) {
+    return {
+      todos: responseBody.todos.map((todo: unknown) => {
+        if (todo === null && typeof todo !== "object") {
+          throw new Error("Invalid todo from API");
+        }
+
+        const { id, done, date, content } = todo as {
+          id: string;
+          content: string;
+          date: string;
+          done: string;
+        };
+
+        return {
+          id,
+          content,
+          done: Boolean(done),
+          date: new Date(date),
+        };
+      }),
+    };
+  }
+
+  return {
+    todos: [],
+  };
 }
